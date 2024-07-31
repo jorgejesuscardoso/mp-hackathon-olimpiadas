87ee1b6fe23787da91846113c9c085ad5c519435
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2024-07-31 19:07:40 -0300

    Preparando o deploy

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,8 +1,9 @@
-const URL = 'http://localhost:3030';
+//const URL = 'http://localhost:3030';
+const URL2 = 'https://apis.codante.io/olympic-games'
 
 export const getAll = async (onde: string) => {
   try {
-    const response = await fetch(`${URL}/${onde}`);
+    const response = await fetch(`${URL2}/${onde}`);
     const data = await response.json();
 
     return data;
